4f505b4ad49423575af9a22f8c47700b1aae7d8f
slape <sslape@2u.com>
2021-11-15 15:17:39 -0500

    ansible provisioning complete

--- deploy_scripts/vagrant_cloud.sh
+++ deploy_scripts/vagrant_cloud.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # a precursor to using packer
 
-vagrant up
-vagrant provision
+# vagrant up
+# vagrant provision
 vagrant package -o ./ubuntu-dev.box
 
 vagrant cloud publish slape/ubuntu-dev \
-1.0.0 virtualbox ~/Documents/ubuntu-dev/vagrant/package.box \
+1.0.0 virtualbox ./ubuntu-dev.box \
 -d "a preconfigured developer environment" \
 --version-description "A Blank Base Box" \
 --release --short-description "Needs further provisioning."
+
+rm -rf ubuntu-dev.box
